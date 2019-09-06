ffe9b6ea2c3e4674a1640700031279dc32b98b34
rogerioneo <rogerio.neo@gmail.com>
2019-09-06 17:44:59 -0300

    Aula 2

---
+++ app/src/main/java/br/edu/ifsp/scl/calculadorasdmkt/Calculadora.kt
@@ -0,0 +1,29 @@
+package br.edu.ifsp.scl.calculadorasdmkt
+
+/* Classe de enumeração para constantes de operadores */
+enum class Operador {
+    RESULTADO, ADICAO, SUBTRACAO, MULTIPLICACAO, DIVISAO
+}
+
+/* Singleton que calcula operações aritméticas básicas */
+object Calculadora {
+    // primeiro operando
+    var operando: Float = 0.0f
+
+    // operador que será aplicado entre primeiro e segundo operando
+    var operador: Operador = Operador.RESULTADO
+
+    /* calcula um valor de retorno com base no operando e operador já existentes, novo valor
+     e atualiza valor de operando e operador */
+    fun calcula(valor: Float, operador: Operador): Float {
+        when (this.operador) {
+            Operador.RESULTADO -> operando = valor
+            Operador.ADICAO -> operando += valor
+            Operador.SUBTRACAO -> operando -= valor
+            Operador.MULTIPLICACAO -> operando *= valor
+            Operador.DIVISAO -> operando /= valor
+        }
+        this.operador = operador
+        return operando
+    }
+}
